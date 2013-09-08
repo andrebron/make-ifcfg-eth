714ebfde0f82a8c04e27bac6327c747a5cd82f89
Andre Brongniart <andrebron@gmail.com>
2013-09-07 21:36:23 -0500

    Update make-ifcfg-eth.sh

--- make-ifcfg-eth.sh
+++ make-ifcfg-eth.sh
@@ -5,8 +5,10 @@
 # VARIABLES MUST BE SUPPLEMENTED TO SUIT YOUR NEEDS.
 
 # It will create all the scripts sequentially from the first script as xxx.yyy.zzz.### up to one full /24
-# You MUST replace ### below with the highest /24 fourth octect to create in the sequence.  
+
 # You can also replace the 1 (in c=1)  with the starting fourth octect for the sequence. The c variable (number 1 in this case) is the starting point for the sequence.
+
+# You MUST replace ### below with the highest /24 fourth octect to create in the sequence.  
 
 for ((c=1;c<=###;c++))
 
